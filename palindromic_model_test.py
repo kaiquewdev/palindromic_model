5db5ed0cacd0781adf21993e9c04f10e51ec4aea
Kaique da Silva <kaique.developer@gmail.com>
2017-04-17 13:16:24 -0300

    Included a basic test to the project

---
+++ palindromic_model_test.py
@@ -0,0 +1,14 @@
+'''Palindromic Model Test'''
+
+import numpy as np
+import tensorflow as tf
+
+from palindromic_model import palindromic_encode
+
+class PalindromicEncodeTest(tf.test.TestCase):
+  def testNonPalindromicEncode(self):
+    with self.test_session():
+      self.assertEqual(palindromic_encode(10).all(),np.array([0,1,0]).all())
+ 
+if __name__ == '__main__':
+  tf.test.main()
