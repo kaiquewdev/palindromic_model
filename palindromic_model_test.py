68a0025b23d67a8e3fb6c101933fc79a2fe13894
Kaique da Silva <kaique.developer@gmail.com>
2017-04-18 22:38:06 -0300

    Test for binary encode covered

--- palindromic_model_test.py
+++ palindromic_model_test.py
@@ -3,9 +3,14 @@
 import numpy as np
 import tensorflow as tf
 # palindromic methods
+from palindromic_model import binary_encode
 from palindromic_model import palindromic_encode
 
 class PalindromicEncodeTest(tf.test.TestCase):
+  def testBinaryEncode(self):
+    with self.test_session():
+      self.assertEqual(binary_encode(101,2**10).all(),np.array([101 >> d & 1 for d in range(2**10)]).all())
+
   def testNonPalindromicEncode(self):
     with self.test_session():
       self.assertEqual(palindromic_encode(10).all(),np.array([0,1,0]).all())
