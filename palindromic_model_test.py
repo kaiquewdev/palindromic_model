4c5a8b5d8d73b091d87578f47457586c21106a9a
Kaique da Silva <kaique.developer@gmail.com>
2017-04-20 20:28:40 -0300

    Tests splitted onto specific cases Differing from states

--- palindromic_model_test.py
+++ palindromic_model_test.py
@@ -5,15 +5,30 @@ import tensorflow as tf
 # palindromic methods
 from palindromic_model import binary_encode
 from palindromic_model import palindromic_encode
+from palindromic_model import palindromic
 
-class PalindromicEncodeTest(tf.test.TestCase):
+class BinaryEncodeTest(tf.test.TestCase):
   def testBinaryEncode(self):
     with self.test_session():
       self.assertEqual(binary_encode(101,2**10).all(),np.array([101 >> d & 1 for d in range(2**10)]).all())
 
+class PalindromicEncodeTest(tf.test.TestCase):
   def testNonPalindromicEncode(self):
     with self.test_session():
       self.assertEqual(palindromic_encode(10).all(),np.array([0,1,0]).all())
  
+class PalindromicStatesTest(tf.test.TestCase):
+  def testFirstPureState(self):
+    with self.test_session():
+      self.assertEqual(palindromic(10,0),'10')
+
+  def testFirstPalindromicState(self):
+    with self.test_session():
+      self.assertEqual(palindromic(101,1),'This numbers is a palindromic (101)')
+
+  def testSecondPalindromicState(self):
+    with self.test_session():
+      self.assertEqual(palindromic(1001,2),'This numbers is a palindromic (1001)')
+
 if __name__ == '__main__':
   tf.test.main()
